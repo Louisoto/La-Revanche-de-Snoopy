d26a90c810396bfbfb954f4e6f07f8137bc3de04
Louisoto <148911900+Louisoto@users.noreply.github.com>
2025-03-03 23:09:27 +0100

    Delete affichage niveau4.h

--- affichage niveau4.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Pierre on 03/12/2023.
-//
-
-#ifndef NIVEAU_1_AFFICHAGE_NIVEAU4_H
-#define NIVEAU_1_AFFICHAGE_NIVEAU4_H
-struct coordonnees;
-void creationniveau4(char niveau[11][11], struct coordonnees *coor_snoopy);
-#endif //NIVEAU_1_AFFICHAGE_NIVEAU4_H
