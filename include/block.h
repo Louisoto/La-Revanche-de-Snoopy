256ea8cc06daf034da4ff0b0ee23ae40961dfd58
Louisoto <148911900+Louisoto@users.noreply.github.com>
2025-03-03 23:13:23 +0100

    Update and rename block.h to include/block.h

--- include/block.h
+++ include/block.h
@@ -1,14 +1,11 @@
-//
-// Created by Pierre on 12/11/2023.
-//
+#ifndef BLOCK_H
+#define BLOCK_H
 
-#ifndef NIVEAU_1_BLOCK_H
-#define NIVEAU_1_BLOCK_H
-struct coordonnees;
 char recuperer_block(char niveau[11][11], struct coordonnees *coor_snoopy, char direction);
 int block_poussable_haut(char niveau[11][11], struct coordonnees *coor_snoopy, char direction);
 int block_poussable_bas(char niveau[11][11], struct coordonnees *coor_snoopy, char direction);
 int block_poussable_droite(char niveau[11][11], struct coordonnees *coor_snoopy, char direction);
 int block_poussable_gauche(char niveau[11][11], struct coordonnees *coor_snoopy, char direction);
-int blockcassable(char niveau[11][11], struct coordonnees *coor_snoopy,char direction);
-#endif //NIVEAU_1_BLOCK_H
+void blockcassable(char niveau[11][11], struct coordonnees *coor_snoopy, char direction);
+
+#endif
