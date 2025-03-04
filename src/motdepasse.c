89c8e9aeefdb3b07e4a3ecf3e1aa264e453cb1b4
Louisoto <148911900+Louisoto@users.noreply.github.com>
2025-03-04 08:33:52 +0100

    Update and rename motdepasse.c to src/motdepasse.c

--- src/motdepasse.c
+++ src/motdepasse.c
@@ -1,47 +1,36 @@
-//
-// Created by Nathan on 31/10/2023.
-//
+#include "motdepasse.h"
 #include <stdio.h>
 #include <string.h>
-#include "motdepasse.h"
-#include "niveau 1.h"
+#include "niveau1.h"
 #include "niveau2.h"
-#include "niveau 3.h"
+#include "niveau3.h"
 #include "niveau4.h"
 
-int motdepasse(){
+int motdepasse() {
     char mdpNiv1[20] = "Aramis";
     char mdpNiv2[20] = "Athos";
-    char mdpNiv3[20] = "d'Artagnan";
+    char mdpNiv3[20] = "Artagnan";
     char mdpNiv4[20] = "Porthos";
-    char mdputilisateur[20];
+    char mdputilisateur[30];
 
     printf("Entrez le mot de passe correspondant au niveau sur lequel vous voulez jouer\n");
     scanf("%s", mdputilisateur);
 
     if (strcmp(mdputilisateur, mdpNiv1) == 0) {
         printf("Vous entrez dans le niveau 1\n");
-        niveau_1();
-    }
-
-    else if (strcmp(mdputilisateur, mdpNiv2) == 0) {
+        niveau1();
+    } else if (strcmp(mdputilisateur, mdpNiv2) == 0) {
         printf("Vous entrez dans le niveau 2\n");
-        niveau_2();
-    }
-
-    else if (strcmp(mdputilisateur, mdpNiv3) == 0) {
+        niveau2();
+    } else if (strcmp(mdputilisateur, mdpNiv3) == 0) {
         printf("Vous entrez dans le niveau 3\n");
-        niveau_3();
-    }
-
-    else if (strcmp(mdputilisateur, mdpNiv4) == 0) {
+        niveau3();
+    } else if (strcmp(mdputilisateur, mdpNiv4) == 0) {
         printf("Vous entrez dans le niveau 4\n");
-        niveau_4();
-    }
-
-    else {
+        niveau4();
+    } else {
         printf("Mot de passe incorrect.\n");
     }
-    return 0;
 
-    }
+    return 0;
+}
