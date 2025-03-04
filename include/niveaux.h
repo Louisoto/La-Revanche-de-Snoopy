8515d41028fb206136df097b40a36fc29e4e53f6
Louisoto <148911900+Louisoto@users.noreply.github.com>
2025-03-04 08:58:46 +0100

    Update and rename niveau 3.h to include/niveaux.h

---
+++ include/niveaux.h
@@ -0,0 +1,9 @@
+#ifndef NIVEAUX_H
+#define NIVEAUX_H
+
+int niveau1();
+int niveau2();
+int niveau3();
+int niveau4();
+
+#endif
--- niveau 3.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Pierre on 03/12/2023.
-//
-
-#ifndef NIVEAU_1_NIVEAU_3_H
-#define NIVEAU_1_NIVEAU_3_H
-struct coordonnees;
-int niveau_3();
-#endif //NIVEAU_1_NIVEAU_3_H
