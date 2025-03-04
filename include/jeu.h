54ff0014bfec83cd939e23afb8248db033335c9e
Louisoto <148911900+Louisoto@users.noreply.github.com>
2025-03-04 08:16:00 +0100

    Update and rename jeu.h to include/jeu.h

---
+++ include/jeu.h
@@ -0,0 +1,6 @@
+#ifndef JEU_H
+#define JEU_H
+
+int jeu();
+
+#endif
--- jeu.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Pierre on 03/12/2023.
-//
-
-#ifndef NIVEAU_1_JEU_H
-#define NIVEAU_1_JEU_H
-int jeu();
-#endif //NIVEAU_1_JEU_H
