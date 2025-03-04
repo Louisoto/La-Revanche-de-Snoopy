a46a22202f386c8130da51886115224254f47e0e
Louisoto <148911900+Louisoto@users.noreply.github.com>
2025-03-04 08:28:13 +0100

    Update and rename deplacement.h to src/deplacement.h

---
+++ src/deplacement.h
@@ -0,0 +1,7 @@
+#ifndef DEPLACEMENT_H
+#define DEPLACEMENT_H
+
+char obtenir_deplacement();
+void appliquer_deplacement(char niveau[11][11], struct coordonnees *coor_snoopy, char direction);
+
+#endif
