3c13232e5d04f24d123ec99ab65924560fe271f1
Louisoto <148911900+Louisoto@users.noreply.github.com>
2025-03-04 08:36:14 +0100

    Update and rename procedure niveau.h to include/procedure niveau.h

---
+++ include/procedure niveau.h
@@ -0,0 +1,7 @@
+#ifndef PROCEDURE_NIVEAU_H
+#define PROCEDURE_NIVEAU_H
+
+int procedure_niveau(char niveau[11][11], struct coordonnees *coor_snoopy, int balle);
+int procedure_niveau_charger(char niveau[11][11], struct coordonnees *coor_snoopy, int nb_vie, char timer[6]);
+
+#endif
