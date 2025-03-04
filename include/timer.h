a76561c21d45fcccd01e61a1f5b27c1fede59e6f
Louisoto <148911900+Louisoto@users.noreply.github.com>
2025-03-04 08:41:52 +0100

    Update and rename timer.h to include/timer.h

---
+++ include/timer.h
@@ -0,0 +1,7 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+void creation_timer(char timer[6]);
+int application_timer(char timer[6], clock_t debut);
+
+#endif
--- timer.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Pierre on 12/11/2023.
-//
-
-#ifndef NIVEAU_1_TIMER_H
-#define NIVEAU_1_TIMER_H
-void creation_timer(char timer[6]);
-int application_timer(char timer[6], clock_t debut);
-#endif //NIVEAU_1_TIMER_H
