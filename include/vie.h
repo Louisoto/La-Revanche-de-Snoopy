f485290fe37c29c8f3eeb6a088a847fa1f614505
Louisoto <148911900+Louisoto@users.noreply.github.com>
2025-03-04 08:43:03 +0100

    Update and rename vie.h to include/vie.h

---
+++ include/vie.h
@@ -0,0 +1,7 @@
+#ifndef VIE_H
+#define VIE_H
+
+void creation_vie(char vie[4]);
+void application_vie(char vie[4], int nb_vie);
+
+#endif
--- vie.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Pierre on 12/11/2023.
-//
-
-#ifndef NIVEAU_1_VIE_H
-#define NIVEAU_1_VIE_H
-void creation_vie(char vie[4]);
-void application_vie(char vie[4], int nb_vie);
-#endif //NIVEAU_1_VIE_H
